951b70588260b0327515dd5ae3bc190567ee8f24
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-11-16 02:21:59 -0500

    agregando modificaciones ala api

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,22 @@
 import UserModel from '../Models/userModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+// Get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    let users = await UserModel.find()
+
+    users = users.map(user => {
+      const { password, ...otherDetails } = user._doc
+      return otherDetails
+    })
+
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 // Get Users ByID
 export const getUsersById = async (req, res) => {
@@ -23,9 +40,9 @@ export const getUsersById = async (req, res) => {
 // Update User
 export const updateUser = async (req, res) => {
   const id = req.params.id
-  const { currentUserId, currentUserAdminStatus, password } = req.body
+  const { _id, currentUserAdminStatus, password } = req.body
 
-  if (id === currentUserId || currentUserAdminStatus) {
+  if (id === _id) {
     try {
       if (password) {
         const salt = await bcrypt.genSalt(10)
@@ -36,7 +53,16 @@ export const updateUser = async (req, res) => {
         new: true
       })
 
-      res.status(200).json(user)
+      const token = jwt.sign(
+        {
+          username: user.username,
+          id: user._id
+        },
+        process.env.JWT_KEY,
+        { expiresIn: '1h' }
+      )
+
+      res.status(200).json({ user, token })
     } catch (error) {
       res.status(500).json(error)
     }
@@ -67,17 +93,17 @@ export const deleteUser = async (req, res) => {
 export const followUser = async (req, res) => {
   const id = req.params.id
 
-  const { currentUserId } = req.body
+  const { _id } = req.body
 
-  if (currentUserId === id) {
+  if (_id === id) {
     res.status(403).json('Action forbidden')
   } else {
     try {
       const followUser = await UserModel.findById(id)
-      const followingUser = await UserModel.findById(currentUserId)
+      const followingUser = await UserModel.findById(_id)
 
-      if (!followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $push: { followers: currentUserId } })
+      if (!followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $push: { followers: _id } })
         await followingUser.updateOne({ $push: { following: id } })
         res.status(200).json('User followed!')
       } else {
@@ -93,17 +119,17 @@ export const followUser = async (req, res) => {
 export const UnFollowUser = async (req, res) => {
   const id = req.params.id
 
-  const { currentUserId } = req.body
+  const { _id } = req.body
 
-  if (currentUserId === id) {
+  if (_id === id) {
     res.status(403).json('Action forbidden')
   } else {
     try {
       const followUser = await UserModel.findById(id)
-      const followingUser = await UserModel.findById(currentUserId)
+      const followingUser = await UserModel.findById(_id)
 
-      if (followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $pull: { followers: currentUserId } })
+      if (followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $pull: { followers: _id } })
         await followingUser.updateOne({ $pull: { following: id } })
         res.status(200).json('User Unfollowed!')
       } else {
